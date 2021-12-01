fd44abe2ae122bb4c50186b457c834312a1bb3d7
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-12-01 15:26:09 +0900

    Update number_gui.py

--- game/guess_the_number/number_gui.py
+++ game/guess_the_number/number_gui.py
@@ -1,33 +1,53 @@
 import random
 import sys
-import Tkinter
+import tkinter
 
-root = Tkinter.Tk()
+root = tkinter.Tk()
+box = None
 number = 0
+button = None
 is_playing = False
 count = 0
+font = tkinter.font.Font(root, family="./mushin.otf", size=20, weight="bold")
 
 def generate_number():
     return random.randint(000, 999)
 
 def setup_root():
-    global root
+    global root, box, button
     root.title(u"Guess The Number")
-    root.geometry("400×300")
+    root.geometry("400×150")
     
-    label = Tkinter.Label(
+    label = tkinter.Label(
         text=u"Enter the number (000 - 999)", 
+        font=font
     )
-    label.pack()
+    label.place(
+        x=20, 
+        y=20
+    )
     
-    box = Tkinter.Entry()
-    box.pack()
+    box = tkinter.Entry()
+    box.place(
+        width=4, 
+        x=120, 
+        y=60
+    )
+    
+    button = tkinter.Button(
+        text=u"入力",
+        font=font
+    )
+    button.place(x=120, y=100)
 
 def run():
     global is_playing, count, number
     
+    number = generate_number()
+    is_playing = True
     
     while is_playing == True:
+        
         
         
 if __name__ == "__main__":
