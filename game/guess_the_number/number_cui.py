043230814804602e5f9eef9e09f841324967ea4a
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-12-01 14:53:58 +0900

    add count

--- game/guess_the_number/number_cui.py
+++ game/guess_the_number/number_cui.py
@@ -1,31 +1,37 @@
 import random
 
 number = 0
+count = 0
 is_playing = False
 
 def generate_number():
     return random.randint(000, 999)
 
 def run():
-    global number, is_playing
+    global number, is_playing, count
+    
     number = generate_number()
     is_playing = True
 
     while is_playing == True:
-        i = input("Enter the number (000 〜 999) > ")
+        try:
+            i = input("Enter the number [{count}] (000 〜 999) > ")
+        except:
+            is_playing = False
         
         if not i.isdigit():
             print("number must be a integer")
             continue
         
         i = int(i)
+        count += 1
 
         if number > i:
             print("higher")
         elif number < i:
             print("lower")
         else:
-            print(f"collect! answer is {number}")
+            print(f"collect! answer is {number} ({count})")
             is_playing = False
     print("Game ended.")
 
