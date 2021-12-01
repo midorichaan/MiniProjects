fc31b2ebc1a6bba1e8bb71bc68ba03a1a7ac5ff4
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-12-01 15:08:36 +0900

    add

--- game/guess_the_number/number_gui.py
+++ game/guess_the_number/number_gui.py
@@ -1,11 +1,39 @@
+import random
 import sys
-import pygame
+import Tkinter
+
+root = Tkinter.Tk()
+number = 0
+is_playing = False
+count = 0
+
+def generate_number():
+    return random.randint(000, 999)
+
+def setup_root():
+    global root
+    root.title(u"Guess The Number")
+    root.geometry("400×300")
+    
+    label = Tkinter.Label(
+        text=u"Enter the number (000 - 999)", 
+    )
+    label.pack()
+    
+    box = Tkinter.Entry()
+    box.pack()
 
 def run():
-    pass
-
+    global is_playing, count, number
+    
+    
+    while is_playing == True:
+        
+        
 if __name__ == "__main__":
     try:
+        setup_root()
+        root.mainloop()
         run()
     except Exception as exc:
         print(f"[Error] {exc}")
